d6c05ea15d691836ad0c5733e8040b9ab92461c6
Megan McLaughlin <megan.mclaughlin@users.noreply.github.com>
2017-04-25 11:34:13 -0500

    end of class check-in

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -2,7 +2,7 @@ class PhotosController < ApplicationController
 
 def index
 # can leave out "/photos" because that is the name of the controller
-@all_photos = Photo.all
+@all_photos = Photo.all.order({:created_at => :desc })
     render("index.html.erb")
 end
 
@@ -10,15 +10,45 @@ def show
 # The params has looks like this: {"the_id_number"=>"1"}
 
 id_number = params["the_id_number"]
-p = Photo.find(id_number)
+@photo = Photo.find(id_number)
 
-@the_caption = p.caption
-@the_image_url = p.source
-@created_at = p.created_at
+@the_caption = @photo.caption
+@the_image_url = @photo.source
+@created_at = @photo.created_at
 
     render("show.html.erb")
 end
 
+def new_form
+  render("new_form.html.erb")
+end
+
+def create_row
+
+p = Photo.new
+p.source = params["the_image_url"]
+p.caption = params["the_caption"]
+p.save
+
+# render("create_row.html.erb"), instead use redirect_to
+  redirect_to("/photos")
+
+end
+
+def edit_form
+@photo = Photo.find(params[:the_id])
+  render("edit_form.html.erb")
+end
+
+def update_row
+  p = Photo.find(params[:the_id])
+  p.source = params["the_image_url"]
+  p.caption = params["the_caption"]
+  p.save
+
+  # render("update_row.html.erb"), instead use redirect_to
+    redirect_to("/photos")
+end
 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,17 @@
 Rails.application.routes.draw do
+# CREATE ROUTES
+get("/photos/new", { :controller => "photos", :action => "new_form" })
+get("/create_photo", { :controller => "photos", :action => "create_row" })
 
+# READ ROUTES
 get("/photos/:the_id_number", { :controller => "photos", :action => "show" })
-
 get("/photos", { :controller => "photos", :action => "index" })
 
+# UPDATE ROUTES
+get("/photos/:the_id/edit", { :controller => "photos", :action => "edit_form" })
+get("/update_photo/:the_id", { :controller => "photos", :action => "update_row" })
 
-
-
-
-
-
-
-
-
+# DELETE ROUTES
 
 
 
